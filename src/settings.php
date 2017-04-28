5db54342f5ea1cc3cc4c4406cabb837e3d48ac36
Olof Johansson <olof.johansson@oddhill.se>
2017-04-28 16:07:38 +0200

    Removed obsolete settings.

--- src/settings.php
+++ src/settings.php
@@ -15,29 +15,5 @@ return [
             'path' => __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
-
-        // Harvest settings
-        'harvest' => [
-            'account' => getenv('HARVEST_ACCOUNT'),
-            'user' => getenv('HARVEST_USER'),
-            'password' => getenv('HARVEST_PASSWORD'),
-            'exclude' => [
-                'clients' => explode(',', getenv('HARVEST_EXCLUDE_CLIENTS')),
-                'projects' => explode(',', getenv('HARVEST_EXCLUDE_PROJECTS')),
-            ],
-            'internal' => [
-                'clients' => explode(',', getenv('HARVEST_INTERNAL_CLIENTS')),
-                'projects' => explode(',', getenv('HARVEST_INTERNAL_PROJECTS')),
-            ],
-        ],
-
-        // Slack integration
-        'slack' => [
-          'webhook_url' => getenv('SLACK_WEBHOOK_URL'),
-          'channel' => getenv('SLACK_WEBHOOK_CHANNEL'),
-          'username' => getenv('SLACK_WEBHOOK_USERNAME'),
-          'icon_url' => getenv('SLACK_WEBHOOK_ICON_URL'),
-          'icon_emoji' => getenv('SLACK_WEBHOOK_ICON_EMOJI'),
-        ],
     ],
 ];
